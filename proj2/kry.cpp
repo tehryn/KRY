859d803f5670fbe32ccc5f88436cdf67c29745c2
tehryn <thetehryn@gmail.com>
2020-05-01 23:09:23 +0200

    [FEATURE] generovani, sifrovani, desifrovani

--- proj2/kry.cpp
+++ proj2/kry.cpp
@@ -8,10 +8,10 @@
 #define debug(str,n) std::cerr << __LINE__ << ": " << str << ": " << mpz_get_str( nullptr, FORMAT, n ) << std::endl
 #define print(str) std::cerr << str << std::endl
 
-
 enum Settings     { GENERATE, DECRYPT, ENCRYPT, BREAK, INVALID };
-enum ReturnValues { SUCCESS = 0, INVALID_ARGUMENTS, MPZ_INIT_FAIL, FILE_ACCESS_FAIL };
-const int FORMAT = 10;
+enum ReturnValues { SUCCESS = 0, INVALID_ARGUMENTS, MPZ_INIT_FAIL, FILE_ACCESS_FAIL, INVALID_PARAM_E, INVALID_PARAM_N };
+const int FORMAT    = 16;
+const char * PREFIX = FORMAT == 16 ? "0x" : "";
 
 bool isUnsigned( const std::string & str ) {
     for ( char c : str ) {
@@ -57,7 +57,7 @@ Settings parseArguments( int argc, const char ** argv ) {
     }
     else if ( argc == 5 ) {
         std::string arg = argv[1];
-        if ( !isHexaDecimal( argv[2] ) || !isHexaDecimal( argv[3] ) ) {
+        if ( !isHexaDecimal( argv[2] ) || !isHexaDecimal( argv[3] ) || !isHexaDecimal( argv[4] ) ) {
             return INVALID;
         }
         else if ( arg == "-e" ) {
@@ -74,31 +74,10 @@ Settings parseArguments( int argc, const char ** argv ) {
     return INVALID;
 }
 
-void printResultVector( ReturnValues ret_value, const std::vector<mpz_t> & result ) {
-    if ( ret_value != SUCCESS ) {
-        std::cerr << "Task failed." << std::endl;
-    } 
-    else {
-        // for( unsigned int i = 0; i < result.size(); i++ ) {
-        //     std::cout << result[i];
-        //     if ( i == result.size() - 1 ) {
-        //         std::cout << std::endl;
-        //     }
-        //     else {
-        //         std::cout << ' ';
-        //     }
-        // }
-    }
-    return;
-}
-
-
 ReturnValues randomNumber( mpz_t & result, size_t bits, bool mask = false ) {
     size_t extra = bits % 8;
     size_t size  = ( ( bits + 8 - ( extra > 0 ? extra : 8 ) ) ) >> 3;
     std::vector<char> bytes( size );
-    
-    //std::cout << size << std::endl;
     
     std::ifstream randomSrc( "/dev/urandom", std::ios::out | std::ios::binary );
     if ( !randomSrc.is_open() || !randomSrc.read( bytes.data(), size ) ) {
@@ -121,66 +100,102 @@ ReturnValues randomNumber( mpz_t & result, size_t bits, bool mask = false ) {
     return SUCCESS;
 }
 
-/*
-function powerMod(b, e, m)
-    x := 1
-    while e > 0
-        if e%2 == 1
-            x, e := (x*b)%m, e-1
-        else b, e := (b*b)%m, e//2
-    return x
- */
-
- void gcd( mpz_t & result, const mpz_t & a, const mpz_t & b ) {
-     if ( mpz_cmp( a, b ) < 0 ) {
-         gcd( result, b, a );
-     }
-     else {
-         mpz_t mod;
-         mpz_init( mod );
-         mpz_mod( mod, a, b );
-         if ( mpz_cmp_ui( mod, 0 ) == 0 ) {
-             mpz_set( result, b );
-         }
-         else {
-             gcd( result, b, mod );
-         }
-         mpz_clear( mod );
-     }
- }
-
-bool powerTest( const mpz_t & base, const mpz_t & exp, const mpz_t & mod ) {
-    mpz_t a, b, e, tmp;
-    mpz_inits( a, e, tmp, b, nullptr );
+void invert( mpz_t & result, const mpz_t & num, const mpz_t & modulo ) {
+    if ( mpz_cmp_ui( modulo, 0 ) == 0 ) {
+        mpz_set_ui( result, 0 );
+        return;
+    }
     
-    mpz_set_ui( a, 1 );
-    mpz_set( b, base );
+    mpz_t mod, a, y, q, tmp;
+    mpz_inits( mod, a, y, q, tmp, nullptr );
+    
+    mpz_set( mod, modulo );
+    mpz_set( a, num );
+    
+    mpz_set_ui( result, 1 );
+    mpz_set_ui( y, 0 );
+    
+    while ( mpz_cmp_ui( a, 1 ) > 0 ) {
+        mpz_tdiv_q( q, a, mod );
+        mpz_set( tmp, mod );
+        mpz_mod( mod, a, mod );
+        mpz_set( a, tmp );
+        mpz_set( tmp, y );
+        mpz_mul( y, y, q );
+        mpz_sub( y, result, y );
+        mpz_set( result, tmp );
+    }
+    
+    if ( mpz_cmp_ui( result, 0 ) < 0 ) {
+        mpz_add( result, result, modulo );
+    }
+    
+    mpz_clears( mod, a, y, q, tmp, nullptr );
+} 
+
+void gcd( mpz_t & result, const mpz_t & a, const mpz_t & b ) {
+    if ( mpz_cmp( a, b ) < 0 ) {
+        gcd( result, b, a );
+    }
+    else {
+        mpz_t mod;
+        mpz_init( mod );
+        mpz_mod( mod, a, b );
+        if ( mpz_cmp_ui( mod, 0 ) == 0 ) {
+            mpz_set( result, b );
+        }
+        else {
+            gcd( result, b, mod );
+        }
+        mpz_clear( mod );
+    }
+}
+
+void powm( mpz_t & result, const mpz_t & num, const mpz_t & exp, const mpz_t & modulo ) {
+    mpz_t n;
+    mpz_init( n );
+    mpz_mod( n, num, modulo );
+    if ( mpz_cmp_ui( n, 0 ) == 0 ) {
+        mpz_set_ui( result, 0 );
+        mpz_clear( n );
+        return;
+    }
+    
+    mpz_t odd, e;
+    mpz_inits( odd, e, nullptr );
+    mpz_set_ui( result, 1 );
     mpz_set( e, exp );
     
-    while( mpz_cmp_ui( e, 0 ) > 0 ) {
-        mpz_mod_ui( tmp, e, 2 );
-        if ( mpz_cmp_ui( tmp, 1 ) == 0 ) {
-            mpz_mul( a, a, b );
-            mpz_mod( a, a, mod );
+    while ( mpz_cmp_ui( e, 0 ) > 0 ) {
+        mpz_mod_ui( odd, e, 2 );
+        if ( mpz_cmp_ui( odd, 1 ) == 0 ) {
+            mpz_mul( result, result, n );
+            mpz_mod( result, result, modulo );
         }
-        mpz_mul( b, b, b );
-        mpz_mod( b, b, mod );
+        mpz_mul( n, n, n );
+        mpz_mod( n, n, modulo );
         mpz_div_ui( e, e, 2 );
     }
     
-    mpz_mod( a, a, mod );
+    mpz_clears( odd, e, n, nullptr );
+}
+
+bool powerTest( const mpz_t & num, const mpz_t & exp, const mpz_t & modulo ) {
+    mpz_t a;
+    mpz_init( a );
+    powm( a, num, exp, modulo );
     int ret = mpz_cmp_ui( a, 1 );
-    mpz_clears( a, e, tmp, b, nullptr );
+    mpz_clear( a );
     return ret != 0;
 }
 
 bool isPrime( const mpz_t & n, size_t primeSize, size_t iterations = 30 ) {
-    debug( "Testing", n );
     if ( mpz_cmp_ui( n, 1 ) == 0 ) {
         return false;
     }
     
     mpz_t tmp, n1;
+    bool ret = true;
     mpz_inits( tmp, n1, nullptr );
     mpz_sub_ui( n1, n, 1 );
     
@@ -190,15 +205,110 @@ bool isPrime( const mpz_t & n, size_t primeSize, size_t iterations = 30 ) {
         mpz_add_ui( tmp, tmp, 1 );
         
         if ( powerTest( tmp, n1, n ) ) {
-            return false;
+            ret = false;
+            break;
         }
     }
     
-    return true;
+    mpz_clears( tmp, n1, nullptr );
+    return ret;
+}
+
+void primeFactor( mpz_t & p, mpz_t & q, const mpz_t & n ) {
+    bool p_set = false;
+    bool q_set = false;
+    mpz_t n2, mod, i, sq;
+    mpz_inits( n2, mod, nullptr );
+    mpz_set( n2, n );
+    mpz_mod_ui( mod, n, 2 );
+    
+    while ( mpz_cmp_ui( mod, 0 ) == 0 ) {
+        if ( p_set ) {
+            mpz_set_ui( q, 2 );
+            q_set = true;
+            break;
+        }
+        else {
+            mpz_set_ui( p, 2 );
+            mpz_div_ui( n2, n2, 2 );
+            p_set = true;
+        }
+    }
+    
+    if ( !q_set ) {
+        mpz_sqrt( sq, n );
+        for ( mpz_set_ui( i, 3 ); (mpz_cmp( i, sq ) <= 0) && !q_set; mpz_add_ui( i, i, 2 ) ) {
+            mpz_mod( mod, n2, i );
+            while ( mpz_cmp_ui( mod, 0 ) == 0 ) {
+                if ( p_set ) {
+                    mpz_set( q, i );
+                    q_set = true;
+                    break;
+                }
+                else {
+                    mpz_set( p, i );
+                    mpz_tdiv_q( n2, n2, i );
+                    mpz_mod( mod, n2, i );
+                    p_set = true;
+                }
+            }
+        }
+        
+        if ( p_set && !q_set && mpz_cmp_ui( n2, 2 ) > 0 ) {
+            mpz_set( q, n2 );
+            q_set = true;
+        }
+    }
+    
+    if ( !q_set || !p_set ) {
+        mpz_set( q, 0 );
+        mpz_set( p, 0 );
+    }
+    
+    mpz_clears( n2, mod, nullptr );
+}
+
+ReturnValues computeKeys( const mpz_t & p, const mpz_t & q, mpz_t & e, mpz_t & d, bool skip_e = false ) {
+    mpz_t p1, q1, phi, g, mul, mod;
+    mpz_inits( p1, q1, phi, g, mul, mod, nullptr );
+    
+    mpz_sub_ui( p1, p, 1 );
+    mpz_sub_ui( q1, q, 1 );
+    mpz_lcm( phi, p1, q1 );
+    
+    bool skipped = false;
+    ReturnValues ret = SUCCESS;
+    do {
+        do {
+            do {
+                if ( !skip_e ) {
+                    ret = randomNumber( e, mpz_sizeinbase( phi, 2 ) );
+                    if ( ret != SUCCESS ) {
+                        goto clean;
+                    }
+                }
+                else if ( skipped ) {
+                    ret = INVALID_PARAM_E;
+                    goto clean;
+                }
+                else {
+                    skipped = true;
+                }
+            } while ( mpz_cmp_ui( e, 1 ) <= 0 || mpz_cmp( phi, e ) <= 0 );
+            gcd( g, e, phi );
+        } while ( mpz_cmp_ui( g, 1 ) != 0 );
+        invert( d, e, phi) ;
+        mpz_mul( mul, e, d );
+        mpz_mod( mod, mul, phi );
+    } while ( mpz_cmp_ui( mod, 1 ) != 0 );
+    
+    clean:
+        mpz_clears( p1, q1, phi, g, mul, mod, nullptr );
+    
+    return ret;
 }
 
 ReturnValues generate_key( size_t b, mpz_t & p, mpz_t & q, mpz_t & n, mpz_t & e, mpz_t & d ) {
-
     size_t sizep = ( b >> 1 ) + b % 2;
     size_t sizeq = ( b >> 1 );
     do {
@@ -206,56 +316,42 @@ ReturnValues generate_key( size_t b, mpz_t & p, mpz_t & q, mpz_t & n, mpz_t & e,
         if ( test != SUCCESS ) {
             return test;
         }
-        print( "==========================" );
     } while ( !isPrime( p, sizep ) );
-    debug( "Prime", p );
     
     do {
         ReturnValues test = randomNumber( q, sizeq, true );
         if ( test != SUCCESS ) {
             return test;
         }
-        print( "===========================" );
     } while ( !isPrime( q, sizeq ) );
-    debug( "Prime", q );    
-    /*
+    
     mpz_mul( n, p, q );
-    
-    mpz_t p_1, q_1, phi, gcd, mul, mod;
-    mpz_inits(p_1, q_1, phi, gcd, mul, mod, NULL);
-    
-    mpz_sub_ui(p_1, p, 1);
-    mpz_sub_ui(q_1, q, 1);
-    mpz_lcm(phi, p_1, q_1);
-    
-    gmp_randstate_t x;
-    gmp_randinit_mt( x );
-    do {
-        do {
-            do {
-                mpz_urandomm( e, x, phi );
-            } while ( mpz_cmp_ui(e, 1) <= 0 || mpz_cmp(phi, e) <= 0 );
-            mpz_gcd(gcd, e, phi);
-        } while ( mpz_cmp_ui(gcd, 1) != 0 );
-        mpz_invert(d, e, phi);
-        mpz_mul(mul, e, d);
-        mpz_mod(mod, mul, phi);
-    } while ( mpz_cmp_ui( mod, 1 ) != 0 );
-    mpz_clears(p_1, q_1, phi, gcd, mul, mod, NULL);
-    */
+    return computeKeys( p, q, e, d );
+}
+
+ReturnValues encrypt( mpz_t & result, const mpz_t & e, const mpz_t & n, const mpz_t & message ) {
+    powm( result, message, e, n);
     return SUCCESS;
 }
 
-ReturnValues encrypt( const mpz_t & e, const mpz_t & n, const std::vector<char> & message, std::vector<char> & result ) {
+ReturnValues decrypt( mpz_t & result, const mpz_t & d, const mpz_t & n, const mpz_t & message ) {
+    powm( result, message, d, n);
     return SUCCESS;
 }
 
-ReturnValues decrypt( const mpz_t & d, const mpz_t & n, const std::vector<char> & message, std::vector<char> & result ) {
-    return SUCCESS;
-}
-
-ReturnValues unlimitedPower( const mpz_t & e, const mpz_t & n, const std::vector<char> & message, std::vector<char> & result ) {
-    return SUCCESS;
+ReturnValues unlimitedPower( mpz_t & p, mpz_t & q, mpz_t & decrypted, mpz_t & e, const mpz_t & n, const mpz_t & encrypted ) {
+    primeFactor( p, q, n );
+    if ( mpz_cmp_ui( p, 0 ) == 0 || mpz_cmp_ui( q, 0 ) == 0 ) {
+        return INVALID_PARAM_N;
+    }
+    mpz_t d;
+    mpz_init( d );
+    ReturnValues ret = computeKeys( p, q, e, d );
+    if ( ret == SUCCESS ) {
+        ret = decrypt( decrypted, d, n, encrypted );
+    }
+    mpz_clear( d );
+    return ret;
 }
 
 int main( int argc, const char ** argv ) {
@@ -267,34 +363,59 @@ int main( int argc, const char ** argv ) {
         mpz_inits( p, q, n, e, d, nullptr );
         ret_value = generate_key( std::atoi( argv[2] ), p, q, n, e, d );
         if ( ret_value == SUCCESS ) {
-            std::string p_str( mpz_get_str( nullptr, FORMAT, p ) ),
-                        q_str( mpz_get_str( nullptr, FORMAT, q ) ),
-                        n_str( mpz_get_str( nullptr, FORMAT, n ) ),
-                        e_str( mpz_get_str( nullptr, FORMAT, e ) ),
-                        d_str( mpz_get_str( nullptr, FORMAT, d ) );
-            std::cout << p_str << ' ' << q_str << ' ' << n_str << ' ' << e_str << ' ' << d_str << std::endl;
+            char * p_str = mpz_get_str( nullptr, FORMAT, p );
+            char * q_str = mpz_get_str( nullptr, FORMAT, q );
+            char * n_str = mpz_get_str( nullptr, FORMAT, n );
+            char * e_str = mpz_get_str( nullptr, FORMAT, e );
+            char * d_str = mpz_get_str( nullptr, FORMAT, d );
+            std::cout << PREFIX << p_str << ' ' << PREFIX << q_str << ' ' << PREFIX << n_str << ' ' << PREFIX << e_str << ' ' << PREFIX << d_str << std::endl;
+            free( p_str );
+            free( q_str );
+            free( n_str );
+            free( e_str );
+            free( d_str );
         }
         mpz_clears( p, q, n, e, d, nullptr );
     }
     else if ( mode == DECRYPT ) {
-        mpz_t d, n;
-        mpz_inits( d, n, nullptr );
-        int flag1 = mpz_set_str( d, argv[2], 16 );
-        int flag2 = mpz_set_str( n, argv[3], 16 );
-        std::vector<char> result;
-        std::vector<char> message;
-        if ( flag1 && flag2 ) {
-            ret_value = decrypt( d, n, message, result );
+        mpz_t d, n, message, result;
+        mpz_inits( d, n, message, result, nullptr );
+        int flag1 = mpz_set_str( d, argv[2] + 2, 16 );
+        int flag2 = mpz_set_str( n, argv[3] + 2, 16 );
+        int flag3 = mpz_set_str( message, argv[4] + 2, 16 );
+        if ( !flag1 && !flag2 && !flag3 ) {
+            ret_value = decrypt( result, d, n, message);
+            if ( ret_value  == SUCCESS ) {
+                char * decrypted( mpz_get_str( nullptr, FORMAT, result ) );
+                std::cout << PREFIX << decrypted << std::endl;
+                free( decrypted );
+            }
         }
         else {
             ret_value = MPZ_INIT_FAIL;
             std::cerr << "Unable to init MPZ numbers." << std::endl;
         }
-        mpz_clear(d);
-        mpz_clear(n);
+        mpz_clears( d, n, message, result, nullptr );
     }
     else if ( mode == ENCRYPT ) {
-        
+        mpz_t e, n, message, result;
+        mpz_inits( e, n, message, result, nullptr );
+        int flag1 = mpz_set_str( e, argv[2] + 2, 16 );
+        int flag2 = mpz_set_str( n, argv[3] + 2, 16 );
+        int flag3 = mpz_set_str( message, argv[4] + 2, 16 );
+        if ( !flag1 && !flag2 && !flag3 ) {
+            ret_value = encrypt( result, e, n, message);
+            if ( ret_value  == SUCCESS ) {
+                char * encrypted( mpz_get_str( nullptr, FORMAT, result ) );
+                std::cout << PREFIX << encrypted << std::endl;
+                free( encrypted );
+            }
+        }
+        else {
+            ret_value = MPZ_INIT_FAIL;
+            std::cerr << "Unable to init MPZ numbers." << std::endl;
+        }
+        mpz_clears( e, n, message, result, nullptr );
     }
     else if ( mode == BREAK ) {
         
--- proj2/testrunner.sh
+++ proj2/testrunner.sh
@@ -52,17 +52,9 @@ C="0x30" # encrypt: m^e % n, decrypt: c^d % n
 make clean
 make
 
-echo "Testing multiplicative inverse"
-test_mod_mult_inverse 17 120 113
-test_mod_mult_inverse 59 120 59
-test_mod_mult_inverse 21 100 81
-test_mod_mult_inverse 29 144 5
-test_mod_mult_inverse 55 144 55
-
-
 echo "Testing -g"
 out=$(./kry -g $BITLEN)
-test_out "$out" "0x[0-9a-f] 0x[0-9a-f]* 0x[0-9a-f]* 0x[0-9a-f]* 0x[0-9a-f]*"
+test_out "$out" "0x[0-9a-f]* 0x[0-9a-f]* 0x[0-9a-f]* 0x[0-9a-f]* 0x[0-9a-f]*"
 
 echo "Testing -e"
 out=$(./kry -e $E $N $M)
@@ -74,6 +66,34 @@ test_out "$out" "$M"
 
 echo "Testing -g, -e, -d"
 out=$(./kry -g 4)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 8)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 16)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 32)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 64)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 512)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 1024)
+c_test=$(encrypt_with_gout $out $M)
+m_test=$(decrypt_with_gout $out $c_test)
+test_out "$m_test" "$M"
+out=$(./kry -g 2048)
 c_test=$(encrypt_with_gout $out $M)
 m_test=$(decrypt_with_gout $out $c_test)
 test_out "$m_test" "$M"
